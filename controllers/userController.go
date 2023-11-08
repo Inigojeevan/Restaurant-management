21ea526e27c929d0eada5179aa938fae88851a10
Inigojeevan <in1442@srmist.edu.in>
2023-11-08 21:31:14 +0530

    just overframe

---
+++ controllers/userController.go
@@ -0,0 +1,36 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func HashPassword(password string) string {
+
+}
+
+//verfiy password
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
+
+}
